0625a4bb6472e904ea24d8b876eb14739c61afa6
EduardoCorazon <corazoneduardo@att.net>
2022-09-18 19:48:57 -0500

    update welcome message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hey!</h1>
+      <h1>I'm Currently Working on my New portfolio but,</h1>
+      <h1>Here's the link to my original portfolio:</h1>
+      <a>https://eduardocorazon.github.io/my-portfolio/</a>
     </div>
   );
 }
