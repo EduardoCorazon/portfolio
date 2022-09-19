2a3f191f76fabb886022ea1b93f231ba84214fed
EduardoCorazon <corazoneduardo@att.net>
2022-09-18 19:51:41 -0500

    href

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>I'm Currently Working on my New portfolio but,</h1>
       <h1>Here's the link to my original portfolio:</h1>
-      <a>https://eduardocorazon.github.io/my-portfolio/</a>
+      <a href="https://eduardocorazon.github.io/my-portfolio/">https://eduardocorazon.github.io/my-portfolio/</a>
     </div>
   );
 }
